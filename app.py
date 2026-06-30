0cb8ff79a06f886763b6c2f2c901cc9ab16fa056
Annika <annika.hogrefe@stud.leuphana.de>
2026-06-30 19:15:55 +0200

    first try

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+# Page Configuration
+st.set_page_config(page_title="PyFit", page_icon="💪")
+
+# App Headers
+st.title("🏋️‍♂️ Your Personal Workout Planner")
+st.write("Welcome! Answer the questions below, and we will build a custom routine for you.")
+
+# visual divider line to keep things clean
+st.divider()
+
+# Question 1: Fitness Goals
+st.header("1. What is your goal?")
+goal = st.selectbox(
+    "Select your primary fitness objective:",
+    ["Build Muscle", "Increase Strength", "Lose Body Fat / Toning", "Improve Endurance"]
+)
+
+# Question 2: Frequency
+st.header("2. Availability")
+days = st.slider(
+    "How many days per week do you want to go to the gym?",
+    min_value=1,
+    max_value=7,
+    value=3  # default starting position of the slider
+)
+
+# Question 3: Muscle Prioritization
+st.header("3. Muscle Focus")
+priorities = st.multiselect(
+    "Are there specific muscle groups you want to prioritize? (Select all that apply)",
+    ["Chest", "Back", "Legs", "Shoulders", "Arms", "Core"]
+)
+
+st.divider()
+
+# The Action Button
+# For now, it just prints a confirmation message when clicked
+if st.button("Generate My Plan", type="primary"):
+    st.success("Inputs received successfully! Ready for the next step.")
---
+++ exercises.py
@@ -0,0 +1,51 @@
+EXERCISE_DB = {
+    
+    #LEG EXERCISES
+    "Barbell Back Squats": {
+        "muscle": "Quads",
+        "equipment": "Free Weights",
+        "difficulty": "Advanced"
+    },
+    "Leg Press": {
+        "muscle": "Quads",
+        "equipment": "Machines",
+        "difficulty": "Beginner"
+    },
+    "Leg Extensions": {
+        "muscle": "Quads",
+        "equipment": "Machines",
+        "difficulty": "Beginner"
+    },
+    "Romanian Deadlifts (RDLs)": {
+        "muscle": "Hamstrings",
+        "equipment": "Free Weights",
+        "difficulty": "Intermediate"
+    },
+    "Seated Leg Curl": {
+        "muscle": "Hamstrings",
+        "equipment": "Machines",
+        "difficulty": "Beginner"
+    },
+    "Barbell Hip Thrusts": {
+        "muscle": "Glutes",
+        "equipment": "Free Weights",
+        "difficulty": "Intermediate"
+
+    },
+    "Machine Hip Thrusts": {
+        "muscle": "Glutes",
+        "equipment": "Machine",
+        "difficulty": "Beginner"
+
+    },
+    "Glute Kickback Machine": {
+        "muscle": "Glutes",
+        "equipment": "Machines",
+        "difficulty": "Beginner"
+    },
+    "Dumbbell Standing Calf Raises": {
+        "muscle": "Calves",
+        "equipment": "Free Weights",
+        "difficulty": "Beginner"
+    }
+}
